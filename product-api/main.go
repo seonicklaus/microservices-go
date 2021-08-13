eb3c0c6f235387920021dc35322b74dcfc18d4ef
seonicklaus <seo.nicklaus@gmail.com>
2021-08-13 16:26:23 +0800

    Modified main.go

--- product-api/main.go
+++ product-api/main.go
@@ -1,37 +1,69 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/nicholasjackson/env"
+	"github.com/seonicklaus/microservices-go/product-api/handlers"
 )
 
+var bindAddress = env.String("BIND_ADDRESS", false, ":9090", "Bind address for the server")
+
 func main() {
-	// requests to the path /goodbye will be handled by this function
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World")
-	})
 
-	// any other requests will be handled by this function
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World")
+	env.Parse()
 
-		// read the body
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("Error reading body", err)
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-			http.Error(w, "Error reading body", http.StatusBadRequest)
+	// create the handlers
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	//create a new serve mux and register handlers
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	// create a new server
+	s := http.Server{
+		Addr:         *bindAddress,      // configure the bind address
+		Handler:      sm,                // set the default handler
+		ErrorLog:     l,                 // set the logger for the server
+		ReadTimeout:  5 * time.Second,   // max time to read request from the client
+		WriteTimeout: 10 * time.Second,  // max time to write response to the client
+		IdleTimeout:  120 * time.Second, // max time for connections using TCP Keep-Alive
+	}
+
+	// trap sigterm or interupt and gracefully shutdown the server
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// start the server
+	go func() {
+		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Listen: %s\n", err)
 		}
+	}()
 
-		// write the response
-		fmt.Fprintf(w, "Hello, %s\n", body)
-	})
+	l.Println("Server starting on port: 9090")
 
-	// listen for connection on all ip addresses (0.0.0.0)
-	// port: 9090
-	log.Println("Starting server")
-	err := http.ListenAndServe(":9090", nil)
-	log.Fatal(err)
+	// block until a signal is received
+	sig := <-sigChan
+	l.Println("Got signal: ", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	if err := s.Shutdown(ctx); err != nil {
+		l.Fatalf("Graceful Shutdown Failed: %+v\n", err)
+	}
+
+	l.Println("Graceful shutdown successful")
 }
